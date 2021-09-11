6bbd0e05eccd25f49ee432f5830cf5c567d60cbf
Shung-Hsi Yu <shung-hsi.yu@suse.com>
2021-09-11 15:40:23 +0800

    bpf: Fix leakage due to insufficient speculative store bypass mitigation (bsc#1188983, bsc#1188985, CVE-2021-34556, CVE-2021-35477).
    - Refresh
      patches.kabi/bpf-prevent-memory-disambiguation-attack.patch.
    - Refresh
      patches.kabi/bpf-prevent-out-of-bounds-speculation-on-pointer-ari.patch.
    
    suse-commit: 15cd45420cfb3b5ccd2e5d6fcbd170047919400f

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -139,7 +139,7 @@ struct bpf_insn_aux_data {
 	int ctx_field_size; /* the ctx field size for load insn, maybe 0 */
 	bool seen; /* this insn was processed by the verifier */
 #ifndef __GENKSYMS__
-	int sanitize_stack_off; /* stack slot to be cleared */
+	bool sanitize_stack_spill; /* subject to Spectre v4 sanitation */
 	u8 alu_state; /* used in combination with alu_limit */
 #endif
 };
