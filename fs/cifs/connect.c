325b28840d1aa596e5d30f92445dc03eb1f7eeb2
Paulo Alcantara <palcantara@suse.de>
2021-11-22 15:53:36 -0300

    cifs: fix memory leak of smb3_fs_context_dup::server_hostname (bsc#1190317).
    suse-commit: 98266bae0dd735f8b04860a62ad2ba1afbcbd0aa

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -4340,6 +4340,7 @@ cifs_cleanup_volume_info_contents(struct smb_vol *volume_info)
 {
 	kfree(volume_info->username);
 	kzfree(volume_info->password);
+	kfree(volume_info->server_hostname);
 	kfree(volume_info->UNC);
 	kfree(volume_info->domainname);
 	kfree(volume_info->iocharset);
